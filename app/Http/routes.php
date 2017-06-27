5c5ec8662843b4a875eef69b132817ebf3be55ca
JOANELLY <joanellygonzalez@gmail.com>
2017-06-27 19:33:04 -0400

    add template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+    Route::get('/admin', 'HomeController@index');
+ 
+});
+
+Route::get('/home', 'HomeController@index');
